dfb77badb6632557971f857493c41651c37ccee0
Ismail Erdem Sirma <isoerdem_99@hotmail.com>
2022-01-10 19:37:36 +0300

    Callback abstraction to handle errors correctly

--- weather-app/app.js
+++ weather-app/app.js
@@ -52,3 +52,27 @@ request({ url : geocodeURL, json: true}, (error, response) => {
     const latitude = response.body.features[0].center[1]
     console.log("long:" + longtitude + ", lati:" + latitude)
 })
+
+
+const geocode = (address, callback) => {
+    const url = 'https://api.mapbox.com/geodcoding/v5/mapbox.places/' + address + '.json?access_token=abcd123'
+
+    request({ url: url, json: true }, (error, response) => {
+        if(error){
+            callback('Unable to connect to location services!', undefined)
+        } else if(response.body.features.length === 0){
+            callback('Unable to find location. Try another search.', undefined)
+        } else {
+            callback(undefined, {
+                latitude: response.body.features[0].center[0],
+                longitude: response.body.features[0].center[1],
+                location: response.body.features[0].place_name
+            })
+        }
+    })
+}
+
+geocode('Philadelphia', (error, data)=> {
+    console.log('Error', error)
+    console.log('Data', data)
+})
