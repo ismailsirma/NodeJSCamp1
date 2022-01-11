e42627b9466fe767594f001c8ea8aee0276efd2b
Ismail Erdem Sirma <isoerdem_99@hotmail.com>
2022-01-11 18:01:25 +0300

    serving up static assets

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,20 +1,35 @@
+const path = require('path')
 const express = require('express')
 
+console.log(__dirname)
+//console.log(__filename)
+console.log(path.join(__dirname, '../public'))
 const app = express()
+const publicDirectoryPath = path.join(__dirname, '../public')
+
+// customize the server to serve out folder
+// serve static files for static directory
+app.use(express.static(publicDirectoryPath))
 
 // app.com
 app.get('', (req, res) => {
-    res.send('Hello express!')
+    res.send('<h1>XYZ App</h1>')
 })
 
 // app.com/help
 app.get('/help', (req, res) => {
-    res.send('How can i help?')
+    res.send({
+        name: 'Ismail',
+        surname: 'Sirma'
+    }, {
+        name: 'Sarah',
+        surname: 'Connor'
+    })
 })
 
 // app.com/about
 app.get('/about', (req, res) => {
-    res.send('About ...')
+    res.send('About')
 })
 
 app.listen(3000, () => {
