e8e5082a1de52f6d427abe6ee0bd90af83d51c5c
Ismail Erdem Sirma <isoerdem_99@hotmail.com>
2022-01-13 19:50:03 +0300

    inserting multiple documents

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -10,8 +10,36 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) =>
     }
 
     const db = client.db(databaseName)
+    // first argument is object to be inserted
+    // second argument is callback function to be called
+    // callback function to track error or result info
+    /*
     db.collection('users').insertOne({
         name: 'Ismail',
         surname: 'Sirma'
+    }, (error, result) => {
+        if(error){
+            return console.log('Unable to insert user')
+        }
+
+        console.log(result.insertedId)
     })
+    */
+
+   db.collection('users').insertMany([
+       {
+        name: 'Ismail',
+        surname: 'Sirma'
+       },
+       {
+        name: 'Dan',
+        surname: 'Brown'
+       }
+   ], (error, result) => {
+       if(error) {
+            return console.log('Unable to insert documents!')
+       }
+
+       console.log(result.insertedIds)
+   })
 })
