12f67691dae6e12459c81f65f991926ac0d0b561
Ismail Erdem Sirma <isoerdem_99@hotmail.com>
2022-01-17 19:57:54 +0300

    Log in for users, new user route has been set up

--- task-manager/index.js
+++ task-manager/index.js
@@ -1,11 +1,11 @@
 const express = require('express')
-//const userRouter = require('./routers/user')
+const userRouter = require('./routers/user')
 
 const app = express()
 const port = process.env.PORT || 3000
 
 app.use(express.json())
-//app.use(userRouter)
+app.use(userRouter)
 
 app.listen(port, () => {
     console.log('Server is up on port '+ port)
