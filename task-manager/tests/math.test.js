731feebe14dcf0583512580cdeb9f226f49c1f81
Ismail Erdem Sirma <isoerdem_99@hotmail.com>
2022-01-18 20:04:08 +0300

    asynchronous function testing

--- task-manager/tests/math.test.js
+++ task-manager/tests/math.test.js
@@ -1,4 +1,4 @@
-const { calculateTip, fahrenheitToCelcius, celciusToFahrenheit } = require('../src/math')
+const { calculateTip, add, fahrenheitToCelcius, celciusToFahrenheit } = require('../src/math')
 
 // first argument is name of the test (also error message)
 // second argument is a function
@@ -24,6 +24,28 @@ test('Should convert 32 F to 0 C', () =>{
 test('Should convert 0 C to 32 F', () =>{
     const degree = celciusToFahrenheit(0)
     expect(degree).toBe(32)
+})
+
+test('Async test demo', (done) => {
+    setTimeout(() => {
+        expect(1).toBe(1)
+        done()
+    }, 2000)
+
+})
+
+// Async
+test('Should add two numbers', (done) => {
+    add(2, 3).then((sum) => {
+        expect(sum).toBe(5)
+        done()
+    })
+})
+
+// Async with await testing
+test('Should add two numbers async/await', async () => {
+    const sum = await add(2, 3)
+    expect(sum).toBe(5)
 })
 
 //test('This should fail', () =>{
