9867185ce692ac47bc21407a8d5eecc45990750e
Ismail Erdem Sirma <isoerdem_99@hotmail.com>
2022-01-18 11:17:18 +0300

    middleware function

--- task-manager/index.js
+++ task-manager/index.js
@@ -5,6 +5,16 @@ const userRouter = require('./routers/user')
 const app = express()
 const port = process.env.PORT || 3000
 
+// middleware function
+app.use((req, res, next) => {
+    if(req.method === 'GET'){
+        res.send('GET requests are disabled')
+    } else {
+        next()
+    }
+    //console.log(req.method, req.path)
+})
+
 app.use(express.json())
 app.use(userRouter)
 
