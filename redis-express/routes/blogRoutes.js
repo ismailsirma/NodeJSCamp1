4077d1a52f0b98f0646de3923e122e2342e2ce07
Ismail Erdem Sirma <isoerdem_99@hotmail.com>
2022-01-31 18:38:08 +0300

    restored blog route handler

--- redis-express/routes/blogRoutes.js
+++ redis-express/routes/blogRoutes.js
@@ -14,33 +14,9 @@ module.exports = app => {
   });
 
   app.get('/api/blogs', requireLogin, async (req, res) => {
-    const redis = require('redis')
-    const redisUrl = 'redis://127.0.0.1:6379'
-    const client = redis.creatClient(redisUrl)
-    const util = require('util')
-    // pass in reference to get function
-    // takes function and returns a new function with promise
-    //overwrite existing function
-    client.get = util.promisify(client.get)
-    
-    // Do we have any cached data in redis related to this query
-    // function returns promise, so we must await
-    const cachedBlogs = await client.get(req.user.id)
-    
-    // if yes, then respond to request with returning cache data
-    if(cachedBlogs){
-      console.log('SERVING FROM CACHE')
-      return res.send(JSON.parse(cachedBlogs))
-    }
-    
-    // if no, we need to get data from database and update cache to store in redis
-    
     const blogs = await Blog.find({ _user: req.user.id })
     
-    console.log('SERVING FROM MONGO DB')
     res.send(blogs)
-    
-  client.set(req.user.id, JSON.stringify(blogs))
   });
 
   app.post('/api/blogs', requireLogin, async (req, res) => {
