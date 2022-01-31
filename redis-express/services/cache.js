341300a92e2ab51474923d5b3f6c640b96151582
Ismail Erdem Sirma <isoerdem_99@hotmail.com>
2022-01-31 23:41:53 +0300

    cache implementation

--- redis-express/services/cache.js
+++ redis-express/services/cache.js
@@ -8,7 +8,7 @@ client.get = util.promisify(client.get)
 const exec = mongoose.Query.prototype.exec
 
 // runs on every single query
-mongoose.Query.prototype.exec = function(){
+mongoose.Query.prototype.exec = async function(){
 	
 	const key = JSON.stringify(
             Object.assign({}, this.getQuery(), {
@@ -16,5 +16,20 @@ mongoose.Query.prototype.exec = function(){
         })
     )
 
-	return exec.apply(this, arguments)
+    // see if we have a value for 'key' in redis, return value
+    const cacheValue = await client.get(key)
+    if(cacheValue){
+        // convert cachedValue in json to a mongoose model
+        const doc = JSON.parse(cacheValue)
+        return Array.isArray(doc) 
+            ? doc.map(d => new this.model(d))
+            : new this.model(JSON.parse(doc))
+    }
+
+	const result = await exec.apply(this, arguments)
+    // if key is not in redis, issue the query and store result in redis
+    client.set(key, JSON.stringify(result))
+
+    //console.log(result.validate)
+    return result
 }
