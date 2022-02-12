ead2b7690a3143031f023c80b1f1fd4e7655690c
Ismail Erdem Sirma <isoerdem_99@hotmail.com>
2022-02-13 00:00:12 +0300

    Automated Cache Clearing with Middleware

--- redis-express/routes/blogRoutes.js
+++ redis-express/routes/blogRoutes.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const requireLogin = require('../middlewares/requireLogin');
-const { clearHash } = require('../services/cache')
+const { cleanCache } = require('../middlewares/cleanCache')
 
 const Blog = mongoose.model('Blog');
 
@@ -21,7 +21,7 @@ module.exports = app => {
     res.send(blogs)
   });
 
-  app.post('/api/blogs', requireLogin, async (req, res) => {
+  app.post('/api/blogs', requireLogin, cleanCache, async (req, res) => {
     const { title, content } = req.body;
 
     const blog = new Blog({
@@ -37,7 +37,5 @@ module.exports = app => {
       res.send(400, err);
     }
 
-    // clear data associated with the user
-    clearHash(req.user.id)
   });
 };
