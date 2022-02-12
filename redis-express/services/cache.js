17349fbd8ff5428eab43d4457f42eb21f868d8ad
Ismail Erdem Sirma <isoerdem_99@hotmail.com>
2022-02-12 16:50:11 +0300

    implement nested hashes

--- redis-express/services/cache.js
+++ redis-express/services/cache.js
@@ -4,8 +4,17 @@ const util = require('util')
 
 const redisUrl = 'redis://127.0.0.1:6379'
 const client = redis.createClient(redisUrl)
-client.get = util.promisify(client.get)
+client.hget = util.promisify(client.hget)
 const exec = mongoose.Query.prototype.exec
+
+mongoose.Query.prototype.cache = function(options={}) {
+    this.useCache = true
+    //top level hash key a number or a string
+    // dont allow empty
+    this.hashKey = JSON.stringify(options.key || '')
+
+    return this
+}
 
 // runs on every single query
 mongoose.Query.prototype.exec = async function(){
@@ -17,7 +26,8 @@ mongoose.Query.prototype.exec = async function(){
     )
 
     // see if we have a value for 'key' in redis, return value
-    const cacheValue = await client.get(key)
+    // get in info from nested hash
+    const cacheValue = await client.hget(this.hashKey, key)
     if(cacheValue){
         // convert cachedValue in json to a mongoose model
         const doc = JSON.parse(cacheValue)
@@ -29,7 +39,8 @@ mongoose.Query.prototype.exec = async function(){
 	const result = await exec.apply(this, arguments)
     // if key is not in redis, issue the query and store result in redis
     // Expiration time of 10 seconds for future sets
-    client.set(key, JSON.stringify(result), 'EX', 10)
+    // setting nested hashkey with hset
+    client.hset(this.hashKey, key, JSON.stringify(result), 'EX', 10)
 
     //console.log(result.validate)
     return result
